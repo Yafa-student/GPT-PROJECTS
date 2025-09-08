439bef60d0f4a7ab84a25e3be369378f24dbf6a2
Yafa-student <hand3e9+new@mbj.comp.il>
2025-09-08 20:05:02 +0300

    Initial commit

---
+++ C#/c#1/c#1/CarCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_1
+{
+    internal class CarCollection<T> where T : Car
+    {
+        public Car[] CarsForSale { get; set; }
+    }
+}
